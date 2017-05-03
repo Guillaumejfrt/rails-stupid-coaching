a84c99c00c69f56189efbced7957b0db58305729
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-03 12:22:28 +0200

    route /ask implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'coaching/answer'
+
+  get '/ask', to: 'coaching#ask'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
